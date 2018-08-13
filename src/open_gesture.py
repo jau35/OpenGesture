ea212e766cd364422bcb7a937317796d1078142c
jau35 <joeurbano14@gmail.com>
2018-08-12 23:29:23 -0400

    background subtraction and thresholding

--- src/open_gesture.py
+++ src/open_gesture.py
@@ -4,14 +4,24 @@ import copy
 import math
 
 class gframe:
+    gaussian_blur_value = 41
+    binary_threshold = 60
+    learning_rate = 0
+
     def __init__(self, arr):
         self.frame = arr
     
     def get(self):
         return self.frame
 
+    def gray(self):
+        self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+
     def blur(self):
-        self.frame = cv2.bilateralFilter(self.frame, 9, 75, 75)
+        self.frame = cv2.GaussianBlur(self.frame, (self.gaussian_blur_value, self.gaussian_blur_value), 0)
+
+    def threshold(self):
+        self.frame = cv2.threshold(self.frame, self.binary_threshold, 255, cv2.THRESH_BINARY)[1]
 
     def flip(self, dir=1):
         self.frame = cv2.flip(self.frame, dir)
@@ -20,6 +30,16 @@ class gframe:
         cv2.namedWindow(title)
         cv2.imshow(title, self.frame)
         return cv2.waitKey(wait)
+
+    def remove_bg(self, bg_model):
+        fgmask = bg_model.apply(self.frame,learningRate=self.learning_rate)
+        kernel = numpy.ones((3, 3), numpy.uint8)
+        fgmask = cv2.erode(fgmask, kernel, iterations=1)
+        self.frame = cv2.bitwise_and(self.frame, self.frame, mask=fgmask)
+    
+    def crop(self, x_begin, x_end, y_begin, y_end):
+        self.frame = self.frame[int(y_begin * self.frame.shape[0]):int(y_end * self.frame.shape[0]), 
+                         int(x_begin * self.frame.shape[1]):int(x_end * self.frame.shape[1])]
         
 class gframe_sequence:
     def __init__(self, seq=[]):
@@ -37,7 +57,7 @@ class gframe_sequence:
     def playback(self, wait=1):
         for frame in self.sequence:
             k = frame.show(title="playback", wait=wait)
-            if(k == 27):
+            if(k == 27): # ESC
                 break
 
 def capture_sequence(camera, num_frames, show_frames=False):
@@ -56,4 +76,16 @@ def capture_sequence(camera, num_frames, show_frames=False):
 
     return sequence
 
+def capture_background(camera, bg_threshold, x_begin, x_end, y_begin, y_end):
+    bg_model = cv2.createBackgroundSubtractorMOG2(0, bg_threshold)
+    
+    # first frame
+    ret, arr = camera.read()
+    f = gframe(arr)
+    f.flip()
+    f.crop(x_begin, x_end, 
+           y_begin, y_end)
+    bg_model.apply(f.get(), learningRate=0)
+    
+    return bg_model
 
--- src/sample.py
+++ src/sample.py
@@ -1,26 +1,46 @@
 import cv2, numpy
-from open_gesture import gframe, gframe_sequence, capture_sequence
+from open_gesture import gframe, gframe_sequence, capture_sequence, capture_background
 from time import sleep
 
-showDuringCapture = True
+# parameters
+begin_x_range=0.5
+end_x_range=1
+begin_y_range=0
+end_y_range=0.8
 
-def countdown(n):
-    print("capturing sequence in...")
+bg_threshold = 50
+
+show_during_capture = False
+
+def countdown(n, msg):
+    print(msg)
     sleep(1)
     print(n)
     for i in range(1, n):
         sleep(1)
         print(n-i)
-
     sleep(1)
 
 camera = cv2.VideoCapture(0)
 camera.set(10,200)
 
-countdown(3)
-
 if camera.isOpened():
-    sequence = capture_sequence(camera, 100, showDuringCapture)
+    countdown(3, "capturing background in...")
+    bg_model = capture_background(camera, bg_threshold,
+                                  x_begin=begin_x_range, x_end=end_x_range, 
+                                  y_begin=begin_y_range, y_end=end_y_range)
+
+    countdown(3, "capturing sequence in...")
+    sequence = capture_sequence(camera, 50, show_during_capture)
+    
+    for frame in sequence:
+        frame.crop(x_begin=begin_x_range, x_end=end_x_range, 
+                   y_begin=begin_y_range, y_end=end_y_range)
+        frame.remove_bg(bg_model)
+        frame.gray()
+        frame.blur()
+        frame.threshold()
+
     sequence.playback(100)
     cv2.destroyAllWindows()
 
